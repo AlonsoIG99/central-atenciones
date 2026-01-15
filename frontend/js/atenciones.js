dcdb4243598a6702757ecaa0a1190a20c8a5bf74
AlonsoIG99 <alonso.ingar@gmail.com>
2026-01-15 17:58:29 -0500

    prestamo evaluacion

--- frontend/js/atenciones.js
+++ frontend/js/atenciones.js
@@ -140,6 +140,7 @@ const atencionSchema = {
   },
   "Apoyo económico/Préstamo": {
     children: {
+      "Evaluación": {},
       "Aprobado": { 
         input: "Monto aprobado",
         select: {
